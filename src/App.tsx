4ff8fd1efed29206080e0d16bddef745f409928f
AntonAthley <antonathley@gmail.com>
2025-02-03 11:08:01 +0100

    react router boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+//pages
+import Home from "./pages/home.tsx";
+import AddTrip from "./pages/AddTrip.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+//layouts
 import "./App.css";
+import NavLayout from "./layouts/NavLayout.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavLayout />}>
+      <Route index element={<Home />} />
+      <Route path="AddTrip" element={<AddTrip />} />
+
+      {/* FIX ME */}
+      {/* <Route path=":id" element={<TripDetails />} loader={tripDetailsLoader} /> */}
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="bg-red-500 ">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/AddTrip.tsx
@@ -0,0 +1,8 @@
+export default function AddTrip() {
+  return (
+    <div className="add-trip">
+      <h2>Lägg till ett resmål</h2>
+      <p>Lorem ipsum dolor</p>
+    </div>
+  );
+}
