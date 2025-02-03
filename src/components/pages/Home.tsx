a855f9348bc4d7d7952ada2d86d27e99d7eb973d
Rhibro <rhibron@gmail.com>
2025-02-03 10:56:09 +0100

    home, header, footer components

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,23 @@
+import Header from '../nav/Header';
+import Footer from '../nav/Footer';
+
+function Home() {
+    return (
+        <>
+           <Header /> 
+            <main>
+                <h1>Travel Planner</h1>
+                <h2>Are you ready to plan your next dream trip?</h2>
+                <div>
+                    <h3>Your Upcoming Trips</h3>
+                        <section className="displayNextTrips">
+                            {/* dynamic display */}
+                        </section>
+                </div>
+            </main>
+           <Footer />
+        </>
+    )
+}
+
+export default Home;
