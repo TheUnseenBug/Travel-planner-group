fb9299b90bec63629760813bdaf4f241cc456eb8
alicegmn <156808426+alicegmn@users.noreply.github.com>
2025-02-05 10:00:54 +0100

    Revert "Alice"

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-//pages
-import Home from "./pages/Home.tsx";
-import AddTrip from "./pages/AddTrip.tsx";
-import NotFound from "./pages/NotFound.tsx";
-
-//layouts
 import "./App.css";
-import NavLayout from "./layouts/NavLayout.tsx";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<NavLayout />}>
-      <Route index element={<Home />} />
-      <Route path="AddTrip" element={<AddTrip />} />
-
-      {/* FIX ME */}
-      {/* <Route path=":id" element={<TripDetails />} loader={tripDetailsLoader} /> */}
-
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+import Home from "./components/pages/Home";
 
 function App() {
-  return <RouterProvider router={router} />;
+
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
