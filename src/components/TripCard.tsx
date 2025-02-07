27404b304e374a0e3652d7d1765f548743c26ea3
alicegmn <alice.eriksson.561@gmail.com>
2025-02-07 10:40:40 +0100

    flexbox

--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -10,12 +10,12 @@ const TripCard: React.FC<TripCardProps> = ({ trip }) => {
   return (
     <Link to={`/trips/${trip.id}`} className="block z-50">
       <div
-        className="bg-gray-400 shadow rounded-3xl
- p-4 mb-4 size-80 border-4 border-white"
+        className="bg-gray-400 shadow rounded-2xl
+ p-4 mb-4 size-80 border-2 border-white hover:scale-95"
       >
-        <h2 className="text-white text-center text-2xl font-bold mb-2">
+        <h3 className="text-white text-center text-2xl font-bold mb-2">
           {trip.city}
-        </h2>
+        </h3>
 
         <img
           className="scale-down rounded-2xl mb-2"
