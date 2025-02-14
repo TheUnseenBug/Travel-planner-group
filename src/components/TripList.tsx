e9a4efcd93ff4e0142f596f75be3f6c12d493f11
AntonAthley <antonathley@gmail.com>
2025-02-14 10:55:18 +0100

    background z index fixed

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -9,11 +9,18 @@ const TripList: React.FC<TripListProps> = ({ trips }) => {
   if (!trips) {
     return <div className="text-white p-4">Du har inga resor inplanerade.</div>;
   }
+
+  const sortedTrips = [...trips].sort((a, b) => {
+    if (!a.date) return 1;
+    if (!b.date) return -1;
+    return new Date(a.date).getTime() - new Date(b.date).getTime();
+  });
+
   return (
     <>
       <h2 className="text-center text-5xl m-5 underline">Dina resor</h2>
       <div className="flex flex-wrap col-end-4 gap-3 justify-center left">
-        {trips.map((trip) => (
+        {sortedTrips.map((trip) => (
           <TripCard key={trip.id} trip={trip} />
         ))}
       </div>
