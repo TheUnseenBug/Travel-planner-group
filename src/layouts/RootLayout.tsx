40bc94bec123e730ef9ff31bff11c7eeda14ca41
AntonAthley <antonathley@gmail.com>
2025-02-14 11:57:44 +0100

    update background

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,13 +2,17 @@ import { Outlet } from "react-router-dom";
 import "../components/nav/Navbar.css";
 import { Navbar } from "../components/nav/Header";
 import Footer from "../components/nav/Footer";
+import RotatingBackground from "../components/RotatingBackground";
 
 export default function RootLayout() {
   return (
     <div className="nav-layout w-full font-mono">
       <Navbar />
       <main>
-        <Outlet />
+        <RotatingBackground>
+          {" "}
+          <Outlet />
+        </RotatingBackground>
       </main>
       <Footer />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,13 @@ import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App.tsx";
 import { store, persistor } from "./stores/trip-store.ts";
-import RotatingBackground from "./components/RotatingBackground.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RotatingBackground>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </RotatingBackground>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
